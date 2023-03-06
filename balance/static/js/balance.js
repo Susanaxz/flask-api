c0368f1e35e823855528141963c7ad76dfa88967
Susana_xz <riberasusana@gmail.com>
2023-03-06 23:19:31 +0100

    feat: ya muestra la bbdd desde el servidor

--- balance/static/js/balance.js
+++ balance/static/js/balance.js
@@ -1,18 +1,42 @@
-const peticion = new XMLHttpRequest() // esto hace una llamada
-
-
-
+const peticion = new XMLHttpRequest();
+console.log("Empiezo a ejecutar JS");
 
 function cargarMovimientos() {
-    console.log("Has llamado a la función cargarMovientos()");
+  console.log("Has llamado a la función cargarMovimientos()");
 
-    peticion.open('GET', 'http://127.0.0.1:5000//api/v1/movimientos', false); //abre una conexion donde se le diga
-    peticion.send();
-    console.log(peticion.responseText);
+  peticion.open(
+    "GET",
+    "http://127.0.0.1:5000/api/v1/movimientos",
+    false
+  );
+  peticion.send();
+  console.log(peticion.responseText);
+  const respuesta = JSON.parse(peticion.responseText);
+  const movimientos = respuesta.results;
+
+  let html = "";
+  for (let i = 0; i < movimientos.length; i = i + 1) {
+    const mov = movimientos[i];
+    console.log("Movimiento", mov);
+    html =
+      html +
+      `
+      <tr>
+        <td>${mov.fecha}</td>
+        <td>${mov.concepto}</td>
+        <td>${mov.tipo}</td>
+        <td>${mov.cantidad}</td>
+      </tr>
+    `;
+  }
+
+  const tabla = document.querySelector("#cuerpo-tabla");
+  tabla.innerHTML += html;
 }
 
 window.onload = function () {
-    console.log("Has llamado a la función anónima al finalizar la carga de la ventana");
-    const boton = document.querySelector('#boton-recarga');
-    boton.addEventListener('click', cargarMovimientos);
-}
+  console.log("Función anónima al finalizar la carga de la ventana");
+  const boton = document.querySelector("#boton-recarga");
+  boton.addEventListener("click", cargarMovimientos);
+  cargarMovimientos();
+};
