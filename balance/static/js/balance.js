6abe9b295ba985da210d0d5496a18aff4438695b
Susana_xz <riberasusana@gmail.com>
2023-03-06 22:35:34 +0100

    feat: como se llama a la API desde JAVASCRIPT

--- balance/static/js/balance.js
+++ balance/static/js/balance.js
@@ -1,10 +1,18 @@
+const peticion = new XMLHttpRequest() // esto hace una llamada
+
+
+
+
 function cargarMovimientos() {
     console.log("Has llamado a la función cargarMovientos()");
+
+    peticion.open('GET', 'http://127.0.0.1:5000//api/v1/movimientos', false); //abre una conexion donde se le diga
+    peticion.send();
+    console.log(peticion.responseText);
 }
 
 window.onload = function () {
     console.log("Has llamado a la función anónima al finalizar la carga de la ventana");
     const boton = document.querySelector('#boton-recarga');
     boton.addEventListener('click', cargarMovimientos);
-
 }
