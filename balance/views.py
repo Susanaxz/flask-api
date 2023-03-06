eb368d45aa0cfa69c2638a447b54332768f76741
Susana_xz <riberasusana@gmail.com>
2023-03-06 20:42:09 +0100

    feat: primer endpoint

--- balance/views.py
+++ balance/views.py
@@ -1,8 +1,29 @@
+from flask import jsonify #convierte un objeto en un json
 from . import app
+from .models import DBManager
 
+"""
+Como se trabaja una API REST:
+Verbos y formato de endpoints
+    GET /movimientos ---------> LISTAR movimientos
+    POST /movimientos ---------> CREAR un movimiento nuevo
+    
+    GET /movimientos/1 ---------> LEER el movimiento con ID=1
+    POST /movimientos/1 ---------> ACTUALIZAR el movimiento con ID=1 (sobreescribe todo el objeto)
+    PUT /movimientos/1 ---------> ACTUALIZAR el movimiento con ID=1 (sobreescrir los campos que enviemos, sobreescribe parcialmente)
+    DELETE /movimientos/1 ---------> ELIMINA el movimiento con ID=1 
+    
+    !!!IMPORTANTE!!!
+    Versionar los endpoints (son un contrato)
+    /api/v1/...
+    
+"""
+
+RUTA = app.config.get('RUTA')
 
 @app.route('/')
 def inicio():
-    return (f'la ruta del archivo de datos es: {app.config["RUTA"]}<br>'
-            f' Secret key: {app.config["SECRET_KEY"]}')
-    
+    db = DBManager(RUTA)
+    sql = 'SELECT * FROM movimientos'
+    movimientos = db.consultaSQL(sql)
+    return jsonify(movimientos)
