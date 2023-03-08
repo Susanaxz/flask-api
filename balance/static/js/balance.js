b88e473130cc8ea472b671ddb9b2c9e4bebf5c98
Susana_xz <riberasusana@gmail.com>
2023-03-08 09:16:20 +0100

    feat: cambiamos descripción del tipo de movimiento

--- balance/static/js/balance.js
+++ balance/static/js/balance.js
@@ -1,38 +1,67 @@
+let spinner;
 const peticion = new XMLHttpRequest();
 console.log("Empiezo a ejecutar JS");
 
 function cargarMovimientos() {
   console.log("Has llamado a la función cargarMovimientos()");
+  spinner.classList.remove("off"); // quitamos la clase off al spinner
 
-  peticion.open("GET", "http://127.0.0.1:5000/api/v1/movimientos", false);
+  peticion.open("GET", "http://127.0.0.1:5000/api/v1/movimientos", true);
   peticion.send();
-  console.log(peticion.responseText);
-  const respuesta = JSON.parse(peticion.responseText);
-  const movimientos = respuesta.results;
 
-  let html = "";
-  for (let i = 0; i < movimientos.length; i = i + 1) {
-    const mov = movimientos[i];
-    console.log("Movimiento", mov);
-    html =
-      html +
-      `
-      <tr>
-        <td>${mov.fecha}</td>
-        <td>${mov.concepto}</td>
-        <td>${mov.tipo}</td>
-        <td>${mov.cantidad}</td>
-      </tr>
-    `;
+  console.log("FIN de la función cargarMovimientos()");
+}
+
+function mostrarMovimientos() {
+  console.log("Entramos en la función mostrarMovimientos", this);
+
+  if (this.readyState === 4 && this.status === 200) {
+    console.log("---- TODO OK ----");
+    const respuesta = JSON.parse(peticion.responseText);
+    const movimientos = respuesta.results;
+
+    let html = "";
+    for (let i = 0; i < movimientos.length; i = i + 1) {
+      const mov = movimientos[i];
+      
+      if (mov.tipo === "I") {
+        mov.tipo = "Ingreso";
+      } else if (mov.tipo === "G"){ 
+        mov.tipo = "Gasto";
+      } else {
+        mov.tipo = "----";
+      }
+
+      html =
+        html +
+        `
+        <tr>
+          <td>${mov.fecha}</td>
+          <td>${mov.concepto}</td>
+
+          <td>${mov.tipo}</td>
+          <td>${mov.cantidad}</td>
+        </tr>
+      `;
+    }
+
+    const tabla = document.querySelector("#cuerpo-tabla");
+    tabla.innerHTML = html;
+  } else {
+    console.error("---- Algo ha ido mal en la petición ----");
+    alert("Error al cargar los movimientos");
   }
 
-  const tabla = document.querySelector("#cuerpo-tabla");
-  tabla.innerHTML += html;
+  spinner.classList.add("off"); // añadimos la clase off al spinner
+  console.log("FIN de la función mostrarMovimientos");
 }
 
 window.onload = function () {
   console.log("Función anónima al finalizar la carga de la ventana");
   const boton = document.querySelector("#boton-recarga");
   boton.addEventListener("click", cargarMovimientos);
+  spinner = document.querySelector("#spinner"); // llamamos al spinner del html
+
   cargarMovimientos();
+  peticion.onload = mostrarMovimientos;
 };
