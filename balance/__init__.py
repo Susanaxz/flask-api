1d925b05de5d58d8a13f793f1fa439e732a9bbe0
Susana_xz <riberasusana@gmail.com>
2023-03-13 22:44:33 +0100

    feat: habilitamos las CORS para las rutas de la API

--- balance/__init__.py
+++ balance/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 app.config.from_object('config')
-
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}}) # permite que cualquier origen pueda acceder a la API REST (CORS)
